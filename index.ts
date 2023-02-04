871603a0a4f6f5765409a35f95e78b9d0f49eb5d
7c00 <i@7c00.cc>
2023-02-04 12:53:31 +0800

    适配新版

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import { Tilemap, TileLayer, MarkerLayer } from "@7c00/canvas-tilemap";
+// @ts-ignore
+import dom2img from "dom-to-image";
 
 let accessToken = "";
 
@@ -35,7 +37,7 @@ async function main() {
     // origin: [3568, 6286],
   });
 
-  tilemap.tileLayers.push(
+  tilemap.tileLayers.add(
     // 提瓦特大陆
     new TileLayer(tilemap, {
       minZoom: 10,
@@ -59,7 +61,6 @@ async function main() {
 
   await fetchAccessToken();
   const { record } = await api("icon/get/list", { size: 1000 });
-  const iconSize = 32;
   const icons: Record<string, string> = {};
   for (const i of record) {
     icons[i.name] = i.url;
@@ -75,7 +76,7 @@ async function main() {
 
   async function addMarker(id: number) {
     const markers = await api("marker/get/list_byinfo", { itemIdList: [id] });
-    tilemap.markerLayers.push(
+    tilemap.markerLayers.add(
       new MarkerLayer(tilemap, {
         positions: markers.map((i: any) =>
           i.position.split(",").map((i: string) => parseInt(i))
@@ -87,34 +88,45 @@ async function main() {
   }
 
   function createMarkerImage(url: string) {
-    const canvas = document.createElement("canvas");
-    const canvas2d = canvas.getContext("2d")!;
+    const iconSize = 36 * devicePixelRatio;
+    const padding = 2 * devicePixelRatio;
     const image = new Image();
-    image.src = url;
-    image.addEventListener("load", () => {
-      canvas.width = iconSize;
-      canvas.height = iconSize;
-      const radius = iconSize / 2;
-      canvas2d.arc(radius, radius, radius, 0, 2 * Math.PI);
-      canvas2d.fillStyle = "rgba(255, 255, 255, 0.5)";
-      canvas2d.fill();
-      // 图片都是正方形的情况下 `canvas2d.drawImage(image, 0, 0, iconSize, iconSize)` 就可以了
-      let size = [image.width, image.height];
-      if (image.width > image.height) {
-        size = [iconSize, (size[1] * iconSize) / size[0]];
-      } else {
-        size = [(size[0] * iconSize) / size[1], iconSize];
-      }
-      canvas2d.drawImage(
-        image,
-        (iconSize - size[0]) / 2,
-        (iconSize - size[1]) / 2,
-        size[0],
-        size[1]
-      );
-      tilemap.draw();
+    const dom = document.createElement("div");
+    dom.style.width = `${iconSize}px`;
+    dom.style.height = `${iconSize}px`;
+    dom.style.position = "relative";
+    dom.innerHTML = `
+      <svg width="${iconSize}" height="${iconSize}" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page_01_PC" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="展开/包含切图" transform="translate(-912.000000, -444.000000)">
+            <g id="loc_02_off" transform="translate(902.000000, 434.000000)">
+              <circle id="椭圆形" fill-opacity="0.298595935" fill="#000000" cx="42" cy="42" r="32"></circle>
+              <path
+                d="M42,13 C58.0162577,13 71,25.9837423 71,42 C71,58.0162577 58.0162577,71 42,71 C25.9837423,71 13,58.0162577 13,42 C13,25.9837423 25.9837423,13 42,13 Z M42,20 C29.8497355,20 20,29.8497355 20,42 C20,54.1502645 29.8497355,64 42,64 C54.1502645,64 64,54.1502645 64,42 C64,29.8497355 54.1502645,20 42,20 Z"
+                id="形状结合" fill="#FFFFFF"></path>
+            </g>
+          </g>
+        </g>
+      </svg>
+    `;
+
+    const icon = new Image();
+    icon.width = iconSize - padding * 2;
+    icon.style.position = "absolute";
+    icon.style.top = `${padding}px`;
+    icon.style.left = `${padding}px`;
+    icon.src = url;
+    icon.addEventListener("load", () => {
+      dom2img.toPng(dom).then((url: string) => {
+        image.src = url;
+        tilemap.draw();
+      });
     });
-    return canvas;
+
+    dom.appendChild(icon);
+    document.body.appendChild(dom);
+    return image;
   }
 }
 
